0b42a7139dc480f32f62e71860ed08b961b8000d
hw <brightrain453@gmail.com>
2026-06-07 23:09:50 +0900

    feat: 이미지 검색을 위한 이미지 저장 요청 DTO 설계

--- app/src/main/java/anki/hw/dto/CardImageRequest.java
+++ app/src/main/java/anki/hw/dto/CardImageRequest.java
@@ -1,4 +1,15 @@
 package anki.hw.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class CardImageRequest {
+
+    private String imageUrl;
+    private String thumbnailUrl;
+    private String source;
+    private String author;
+    private String sourceUrl;
 }
