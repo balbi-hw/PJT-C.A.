1894a9731b52afe61fc278d00255e5eb20f58afc
hw <brightrain453@gmail.com>
2026-06-07 23:07:54 +0900

    feat: 이미지 추가를 위한 Card 엔티티 필드 ì및 메서드 추가

--- app/src/main/java/anki/hw/domain/Card.java
+++ app/src/main/java/anki/hw/domain/Card.java
@@ -19,6 +19,19 @@ public class Card {
     private String word;
     private String meaning;
 
+    @Column(length = 1000)
+    private String imageUrl;
+
+    @Column(length = 1000)
+    private String imageThumbnailUrl;
+
+    private String imageSource;
+
+    private String imageAuthor;
+
+    @Column(length = 1000)
+    private String imageSourceUrl;
+
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "deck_id")
     private Deck deck;
@@ -37,6 +50,28 @@ public class Card {
     public void changeContent(String word, String meaning) {
         this.word = word;
         this.meaning = meaning;
+    }
+
+    public void changeImage(
+            String imageUrl,
+            String imageThumbnailUrl,
+            String imageSource,
+            String imageAuthor,
+            String imageSourceUrl
+    ) {
+        this.imageUrl = imageUrl;
+        this.imageThumbnailUrl = imageThumbnailUrl;
+        this.imageSource = imageSource;
+        this.imageAuthor = imageAuthor;
+        this.imageSourceUrl = imageSourceUrl;
+    }
+
+    public void removeImage() {
+        this.imageUrl = null;
+        this.imageThumbnailUrl = null;
+        this.imageSource = null;
+        this.imageAuthor = null;
+        this.imageSourceUrl = null;
     }
 
 }
