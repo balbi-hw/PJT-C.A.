03ca45e69758a5b9245ca76343e83602f9fc4f8f
hw <brightrain453@gmail.com>
2026-06-08 10:08:35 +0900

    remove: django project

--- django/accounts/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
-from django_extensions.db.models import TimeStampedModel
-
-
-class User(AbstractBaseUser, TimeStampedModel):
-
-
-    pass
--- django/flashcards/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from flashcards import views
-
-
-urlpatterns = [
-    path('decks/', views.deck_list),
-    path('decks/<int:deck_pk>/', views.deck_detail),
-]
